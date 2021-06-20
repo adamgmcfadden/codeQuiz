8f580bf1c7370da1efd51e7bdb71d38f5eb665f1
Adam Girard-McFadden <adam_m_20@hotmail.com>
2021-06-20 18:03:48 -0400

    done, ready for submission

--- assets/js/score.js
+++ assets/js/score.js
@@ -6,13 +6,13 @@ Iterate through stored items and display on high scores HTML page
 function allScores() {
   var values = Object.values(localStorage);
   var keyItem = Object.keys(localStorage);
-  console.log(keyItem.length);
 
   for (let i = 0; i < keyItem.length; i++) {
     var highScore = document.querySelector("#high-score");
-    console.log(keyItem[i]);
     highScore.innerHTML +=
       "<h2 id='high-score'class='high-score'>" +
+      (i + 1) +
+      ". " +
       "Initials: " +
       keyItem[i] +
       " - Score: " +
